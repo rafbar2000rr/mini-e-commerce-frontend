62012773b40834a44d6bf6f6223dfd232ea4b905
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-10-20 17:13:03 -0500

    Fix auth.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/': 'http://localhost:5000',
     },
   },
   plugins: [react()],
