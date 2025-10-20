18564237b7f06a1def0d173123b7a859b7cdd74c
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-10-20 17:21:31 -0500

    Fix auth.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
+// ✅ Configuración para Vercel y producción
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
-  base: "/",
-});
+  base: '/',
+})
