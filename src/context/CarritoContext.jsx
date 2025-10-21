4f1affa2669ee54043a774c0ff4e4e5b3749bd4b
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-10-21 01:24:59 -0500

    Fix auth.js

--- src/context/CarritoContext.jsx
+++ src/context/CarritoContext.jsx
@@ -204,3 +204,13 @@ export function CarritoProvider({ children }) {
     </CarritoContext.Provider>
   );
 }
+
+import { useContext } from "react";
+
+export const useCarrito = () => {
+  const context = useContext(CarritoContext);
+  if (!context) {
+    throw new Error("useCarrito debe usarse dentro de un CarritoProvider");
+  }
+  return context;
+};
