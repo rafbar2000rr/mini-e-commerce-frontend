fd935c07b98854e7f871d338d8417b3b2e926d56
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-10-23 17:20:40 -0500

    Corrige ruta frontend

--- src/pages/MiPerfil.jsx
+++ src/pages/MiPerfil.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Swal from "sweetalert2";
+import {Swal} from "sweetalert2";
 
 export default function MiPerfil() {
   const [usuario, setUsuario] = useState({
