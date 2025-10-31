3eeea8860b1829ab0f1cd841748f1b86c97abf31
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-10-31 16:51:54 -0500

    Corrige ruta frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ export default function Navbar() {
           >
             <FaShoppingCart size={18} />
             {carrito.length > 0 && (
-              <span className="absolute -top-2 -right-2 bg-purple-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+              <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
                 {carrito.length}
               </span>
             )}
